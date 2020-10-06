98abc422f2947ca38f31d2609f2afba9b29de6f1
Moti Elmakies <motiphone2003@gmail.com>
2020-10-07 01:12:39 +0300

    fixed spinner screen center

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Spinner from './Spinner';
 const App = () => {
-  return <Spinner />;
+  return <Spinner type='spinner' />;
 };
 
 export default App;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './spinner.css';
 
-const Spinner = () => {
-  return (
-    <div className='spinner-container'>
-      <div className='spinner'></div>
-    </div>
-  );
+const Spinner = ({ type }) => {
+  const renderedSpinner = () => {
+    switch (type) {
+      case 'spinner': {
+        return <div className='spDefault spinner'></div>;
+        break;
+      }
+      case 'dots': {
+        return (
+          <div className='spDefault dots'>
+            <div className='dot'></div>
+            <div className='dot bounce-1'></div>
+            <div className='dot bounce-2'></div>
+          </div>
+        );
+      }
+    }
+  };
+  return <div className='spinner-container'>{renderedSpinner()}</div>;
 };
 
 export default Spinner;
